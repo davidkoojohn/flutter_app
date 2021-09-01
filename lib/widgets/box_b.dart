614d9dfadab6593bb24bf72b5530142fd2eff0b2
guozhao <davidkoojohn@163.com>
2021-09-01 22:37:19 +0800

    混合状态管理

--- lib/widgets/box_b.dart
+++ lib/widgets/box_b.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
 
-class BoxB extends StatelessWidget {
+// 混合状态管理
+class BoxB extends StatefulWidget {
   BoxB({Key? key, this.active: false, required this.onChanged}) : super(key: key);
 
   final bool active;
   final ValueChanged<bool> onChanged;
 
+  @override
+  _BoxBState createState() => _BoxBState();
+}
+
+class _BoxBState extends State<BoxB> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
   void _handleTap() {
-    onChanged(!active);
+    widget.onChanged(!widget.active);
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _handleTap,
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
       child: Container(
         child: Center(
           child: Text(
-            active ? 'Active' : 'Inactive',
+            widget.active ? 'Active' : 'Inactive',
             style: const TextStyle(
                 fontSize: 32.0,
                 color: Colors.amber
@@ -27,7 +56,8 @@ class BoxB extends StatelessWidget {
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: active ? Colors.lightBlue[700] : Colors.grey[600],
+          color: widget.active ? Colors.lightBlue[700] : Colors.grey[600],
+          border: _highlight ? Border.all(color: Colors.yellow, width: 10.0) : null,
         ),
       ),
     );
