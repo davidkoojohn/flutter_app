9be166ee23c5e57ee21faa1c74843184a8893100
guozhao <davidkoojohn@163.com>
2021-09-03 12:32:19 +0800

    ConstrainedBox Widget

--- lib/routes/box.dart
+++ lib/routes/box.dart
@@ -9,6 +9,11 @@ class BoxPage extends StatefulWidget {
 }
 
 class _BoxPageState extends State<BoxPage> {
+
+  Widget redBox = const DecoratedBox(
+    decoration: BoxDecoration(color: Colors.red),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +23,68 @@ class _BoxPageState extends State<BoxPage> {
       body: Padding(
         padding: const EdgeInsets.all(0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                minHeight: 100,
+                minWidth: 60,
+              ),
+              // UnconstrainedBox不会对子组件产生任何限制
+              // 很少直接使用此组件，但在"去除"多重限制的时候会有帮助
+              child: UnconstrainedBox(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(minWidth: 100.0, minHeight: 20.0),//子
+                  child: redBox,
+                ),
+              ),
+            ),
+            // 多个父级ConstrainedBox限制
+            // minWidth和minHeight来说，是取父子中相应数值较大的
+            // maxWidth和maxHeight来说，是取父子中相应数值较小的
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 100.0,
+                maxWidth: 200.0,
+              ),
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxHeight: 200.0,
+                  maxWidth: 100.0
+                ),
+                child: Container(
+                  height: 300.0,
+                  width: 300.0,
+                  child: redBox,
+                ),
+              ),
+            ),
+            const Divider(),
+            const Divider(),
+            // SizedBox用于给子元素指定固定的宽高
+            SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: redBox,
+            ),
+            const Divider(),
+            ConstrainedBox(
+              constraints: const BoxConstraints.tightFor(width: 100.0, height: 100.0),
+              child: redBox,
+            ),
+            const Divider(),
+            // ConstrainedBox用于对子组件添加额外的约束
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                minWidth: double.infinity,
+                minHeight: 50.0,
+                // maxHeight: 80.0
+              ),
+              child: Container(
+                height: 5.0,
+                child: redBox,
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.fromLTRB(10, 20, 30, 40),
               child: Container(
