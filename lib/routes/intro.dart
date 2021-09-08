079f18e9d420f4ea8f81e5f671483d1ee792280e
guozhao <davidkoojohn@163.com>
2021-09-08 17:55:45 +0800

    refactor intro page

--- lib/routes/intro.dart
+++ lib/routes/intro.dart
@@ -36,32 +36,77 @@ class _IntroPageState extends State<IntroPage> {
           ),
         ],
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Image.asset('assets/images/todaybing.jpg'),
+          Image.asset(
+            'assets/images/todaybing.jpg',
+          ),
           Padding(padding: const EdgeInsets.all(16.0),
             child: Row(
               children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Oeschinen Lake Campground',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Kandersteg, Switzerland',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const Icon(Icons.star, color: Colors.red,),
+                const Text('99', style: TextStyle(color: Colors.red),),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Oeschinen Lake Campground',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Kandersteg, Switzerland',
-                      style: TextStyle(
-                        color: Colors.grey[500],
-                      ),
-                    )
+                  children: const <Widget>[
+                    Icon(Icons.call),
+                    Text('CALL'),
+                  ],
+                ),
+                Column(
+                  children: const <Widget>[
+                    Icon(Icons.near_me),
+                    Text('ROUTE'),
+                  ],
+                ),
+                Column(
+                  children: const <Widget>[
+                    Icon(Icons.share),
+                    Text('SHARE'),
                   ],
                 )
               ],
             ),
           ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+                  'Alps. Situated 1,578 meters above sea level, it is one of the '
+                  'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+                  'half-hour walk through pastures and pine forest, leads you to the '
+                  'lake, which warms to 20 degrees Celsius in the summer. Activities '
+                  'enjoyed here include rowing, and riding the summer toboggan run.',
+              softWrap: true,
+            ),
+          ),
+          const Divider(),
           Card(
             child: Column(
               children: [
@@ -121,7 +166,7 @@ class _IntroPageState extends State<IntroPage> {
             ],
           )
         ],
-      )
+      ),
     );
   }
 }
